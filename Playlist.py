4704b6adc01a0814dc746d4e9749c27918ee18e8
Joaopy3 <jpsantostrajano@gmail.com>
2023-11-11 22:27:17 -0300

    Esse é o ínicio do meu projeto, é sobre criar uma playlist e criar uma interface para essa playlist

--- Playlist.py
+++ Playlist.py
@@ -1,9 +1,43 @@
-#Filtra os arquivos que são mp3 na pasta
+
+arquivos = []
 
 import os
 
-def pegarArquivos_mp3_Filtrados(pasta,tipo):
+def pegarArquivos_Filtrados(pasta,tipo):
+
+    global arquivos
+
     arquivos_diretorios = os.listdir(pasta)
     arquivos = ([arquivos for arquivos in arquivos_diretorios if arquivos.endswith(tipo)])
     return "\n".join(arquivos)
-print(pegarArquivos_mp3_Filtrados('D:\Tabata II','mp3'))
+
+def tocarMusicas(indice,pasta):
+
+    import pygame
+
+    def inicializarPlaylist():
+
+        nonlocal indice
+
+        pygame.mixer.init()
+        pygame.mixer.music.load(os.path.join(pasta,arquivos[indice]))
+        pygame.mixer.music.set_volume(0.6)
+        pygame.time.delay(1)
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            pygame.time.Clock().tick(10)
+
+    def continuarPlaylist():
+        try:
+            while True:
+                inicializarPlaylist()
+                nonlocal indice
+                indice += 1
+        except IndexError:
+            pygame.quit()
+
+    continuarPlaylist()
+
+tocarMusicas(0,caminho)
+
+
