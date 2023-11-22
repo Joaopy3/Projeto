6f0f68247fc4252c16d76f31ae276070d31897c7
Joaopy3 <jpsantostrajano@gmail.com>
2023-11-22 03:00:15 -0300

    Esse é o ínicio do meu projeto, é sobre criar uma playlist e criar uma interface para essa playlist

--- Playlist.py
+++ Playlist.py
@@ -3,6 +3,8 @@ arquivos = []
 
 import os
 
+import pygame
+
 def pegarArquivos_Filtrados(pasta,tipo):
 
     global arquivos
@@ -13,19 +15,38 @@ def pegarArquivos_Filtrados(pasta,tipo):
 
 def tocarMusicas(indice,pasta):
 
-    import pygame
+    import keyboard
 
     def inicializarPlaylist():
 
         nonlocal indice
 
+        busy = True
+        pausa = False
         pygame.mixer.init()
         pygame.mixer.music.load(os.path.join(pasta,arquivos[indice]))
         pygame.mixer.music.set_volume(0.6)
         pygame.time.delay(1)
         pygame.mixer.music.play()
-        while pygame.mixer.music.get_busy():
+        while pygame.mixer.music.get_busy() or busy:
             pygame.time.Clock().tick(10)
+            if keyboard.is_pressed('f2'):
+                pygame.mixer.music.pause()
+                busy = True
+                pausa = True
+            if keyboard.is_pressed('f4'):
+                pygame.mixer.music.unpause()
+                busy = True
+                pausa = False
+            if keyboard.is_pressed('f7'):
+                pygame.mixer.music.stop()
+                busy = False
+            if keyboard.is_pressed('f8'):
+                pygame.mixer.music.stop()
+                busy = False
+                indice -= 2
+            if not pygame.mixer.music.get_busy() and not pausa:
+                break
 
     def continuarPlaylist():
         try:
@@ -38,6 +59,6 @@ def tocarMusicas(indice,pasta):
 
     continuarPlaylist()
 
-tocarMusicas(0,caminho)
+
 
 
